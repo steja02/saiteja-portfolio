8213fe8362d575625c989fcf8bfbc8b5dbcc4620
steja02 <75718137+steja02@users.noreply.github.com>
2025-04-15 23:29:32 +0530

    bring absolute hovering button up by 10px

--- src/components/hero/ScrollIndicator.tsx
+++ src/components/hero/ScrollIndicator.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ScrollIndicator: React.FC = () => {
   return (
-    <div className="absolute bottom-[-120px] left-1/2 -translate-x-1/2 animate-bounce">
+    <div className="absolute bottom-[-50px] left-1/2 -translate-x-1/2 animate-bounce">
       <a href="#about" className="block group">
         <div className="w-10 h-10 border-2 border-devops-highlight rounded-full flex items-center justify-center transition-all duration-300 group-hover:border-opacity-100 group-hover:animate-glow">
           <div className="w-1 h-3 bg-devops-highlight rounded-full"></div>
