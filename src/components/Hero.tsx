7dec18d17131c66cd916799162cca5c08ae0fb9e
steja02 <75718137+steja02@users.noreply.github.com>
2025-04-15 23:01:20 +0530

    change Hover button position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -171,7 +171,7 @@ const Hero = () => {
           </div>
         </div>
         
-        <div className="absolute bottom-4 left-1/2 -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-[-60px] left-1/2 -translate-x-1/2 animate-bounce">
           <a href="#about" className="block group">
             <div className="w-10 h-10 border-2 border-devops-highlight rounded-full flex items-center justify-center transition-all duration-300 group-hover:border-opacity-100 group-hover:animate-glow">
               <div className="w-1 h-3 bg-devops-highlight rounded-full"></div>
